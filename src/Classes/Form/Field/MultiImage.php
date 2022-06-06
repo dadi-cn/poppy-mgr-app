4844a5a1f7c786a21a2b7b74d159f935d137bd01
多厘 <zhaody901@126.com>
2022-06-06 21:25:33 +0800

    feat: 图片上传的类型约定

--- src/Classes/Form/Field/MultiImage.php
+++ src/Classes/Form/Field/MultiImage.php
@@ -4,4 +4,9 @@ namespace Poppy\MgrApp\Classes\Form\Field;
 
 final class MultiImage extends MultiFile
 {
+    public function __construct(string $name, string $label)
+    {
+        parent::__construct($name, $label);
+        $this->image();
+    }
 }
