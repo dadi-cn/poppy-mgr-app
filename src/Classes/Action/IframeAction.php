ff0b0af9c7675f427b05afe16b3e4a2089f9db02
多厘 <zhaody901@126.com>
2022-06-24 19:38:46 +0800

    feat: 支持 iframe action

--- src/Classes/Action/IframeAction.php
+++ src/Classes/Action/IframeAction.php
@@ -8,6 +8,18 @@ namespace Poppy\MgrApp\Classes\Action;
 final class IframeAction extends Action
 {
 
+    protected int $width = 400;
+
+    /**
+     * 预览
+     * @param int $width 宽度
+     * @return void
+     */
+    public function width(int $width = 400)
+    {
+        $this->width = $width;
+    }
+
     /**
      * Action  渲染
      * @return array
@@ -16,6 +28,7 @@ final class IframeAction extends Action
     {
         return array_merge(parent::struct(), [
             'method' => 'iframe',
+            'width'  => 400,
         ]);
     }
 }
